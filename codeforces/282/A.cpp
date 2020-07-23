cef9312a659205ce813afe91e9acc2e642b9ba9e
Aashish Illa <illa.aashish999@gmail.com>
2020-07-23 17:51:00 +0530

    Add solution for problem `A - Bit++` Link: https://codeforces.com/contest/282/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/282/submission/87769901

---
+++ codeforces/282/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+#define floop(a,n) for(int i=a;i<n;i++)
+#define floop_equal(a,n) for(int i=a;i<=n;i++)
+#define array_in(arr,n) for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define array_out(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]
+#define ll long long int
+#define endl "\n"
+#define space " "
+#define TLE_na_ho ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE 400
+
+int main(){
+	int n;
+	cin>>n;
+	int x=0;
+	while(n--){
+		string s;
+		cin>>s;
+
+		if(s[1]=='+')
+			x++;
+		else
+			x--;
+	}
+	cout<<x;
+	
+}
