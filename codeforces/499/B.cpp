7183bccff5e2c5769bc48d1859015c88cd0c5d6b
Aashish Illa <illa.aashish999@gmail.com>
2020-08-17 23:59:00 +0530

    Add solution for problem `B - Lecture` Link: https://codeforces.com/contest/499/problem/B Tags: implementation, strings, *1000 Ref: https://codeforces.com/contest/499/submission/90256251

--- codeforces/499/B.cpp
+++ codeforces/499/B.cpp
@@ -1,39 +1,63 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
-map<string,string> mp;
-map<string,int>mpp;
+
+using ll = long long int;
+using pll = pair <string, ll>;
+#define endl "\n"
+#define space " "
+#define TLE_na_ho ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE 400
+#define push_back pb
+#define fi first
+#define se second
+#define f(a,n) for(ll i=a;i<n;i++)
+#define fe(a,n) for(ll i=a;i<=n;i++)
+#define ain(arr,n) for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+int bs(pll s[],string a,ll n){
+	ll low=0,up=n-1,mid,ans;
+	while(low<=up){
+		mid=low+(up-low)/2;
+		int res=0;
+		if(s[mid].first==a)
+			res=1;
+		if(res==1)
+			return s[mid].second;
+		else if(s[mid].first>a)
+			up=mid-1;
+		else
+			low=mid+1;
+	}
+	return -1;
+}
 
 int main()
 {
-    int n,m,i;
-    string s1,s2,s;
-    cin>>n>>m;
-    while(m--)
-
+    TLE_na_ho;
+    ll n, m;
+    cin >> n >> m;
+    pll a[m];
+    string b[m];
+    f(0, m)
     {
-        cin>>s1>>s2;
-        mp[s1]=s2;
-        mp[s2]=s1;
-
-        mpp[s1]=1;
-        mpp[s2]=2;
+        cin >> a[i].first >> b[i];
+        a[i].second=i;
     }
-    while(n--)
-    {
-        cin>>s;
-        if(mpp[s]==1)
-        s1=s,s2=mp[s];
-        else
-          s2=s,s1=mp[s];
+    string c[n];
+    f(0, n)
+    cin >> c[i];
 
-        if(mp[s2].size()>mp[s1].size())
-            cout<<mp[s1];
-        else
-            cout<<mp[s2];
-        if(n)
-            cout<<" ";
-        else
-            cout<<endl;
-    }
+    sort(a,a+m);
+
+    f(0,n){
+    ll pos=bs(a,c[i],m);
+    if(c[i].length()<=b[pos].length())
+    	cout<<c[i]<<space;
+    else
+    	cout<<b[pos]<<space;
+}
+
+
+
 }
