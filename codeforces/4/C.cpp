14d6eef18ac5d5c30b5aa5f1df10f8f56fd417d8
Aashish Illa <illa.aashish999@gmail.com>
2020-08-29 14:30:00 +0530

    Add solution for problem `C - Registration System` Link: https://codeforces.com/contest/4/problem/C Tags: data structures, hashing, implementation, *1300 Ref: https://codeforces.com/contest/4/submission/91270589

---
+++ codeforces/4/C.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define space " "
+#define TLE_na_ho ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE 1024
+#define MOD 1000000007
+#define pb push_back
+#define fi first
+#define se second
+#define f(a,n) for(ll i=a;i<n;i++)
+#define fe(a,n) for(ll i=a;i<=n;i++)
+#define ain(arr,n) for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+
+int main()
+{
+    TLE_na_ho;
+    int n;
+    cin>>n;
+    string s;
+    map<string,int> c;
+    f(0,n){
+    	cin>>s;
+    	if(c[s]==0)
+    		cout<<"OK"<<endl;
+    	else
+    		cout<<s<<c[s]<<endl;
+    	c[s]++;
+    }
+
+}
