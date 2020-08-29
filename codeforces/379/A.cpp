cc2d696bbfe92947fd6756f225d636f515ee097d
Aashish Illa <illa.aashish999@gmail.com>
2020-08-29 16:00:00 +0530

    Add solution for problem `A - New Year Candles` Link: https://codeforces.com/contest/379/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/379/submission/91276540

---
+++ codeforces/379/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define space " "
+#define TLE_na_ho ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE 1024
+#define MOD 1000000007
+#define pb push_back
+#define fi first
+#define se second
+#define f(a,n) for(ll i=a;i<n;i++)
+#define fe(a,n) for(ll i=a;i<=n;i++)
+#define ain(arr,n) for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+
+int main()
+{
+    TLE_na_ho;
+
+    int n,m;
+    cin>>n>>m;
+    int c1=n,c2=0;
+    int hours=n;
+    while(c1>0){
+    	c2+=c1;
+    	c1=c2/m;
+    	c2=c2%m;
+    	hours+=c1;
+    }
+    cout<<hours;
+}
