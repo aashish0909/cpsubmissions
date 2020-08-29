ac38da13c00504265a2a3bf55bf68d67818f1e2c
Aashish Illa <illa.aashish999@gmail.com>
2020-08-29 22:55:00 +0530

    Add solution for problem `B - Interesting drink` Link: https://codeforces.com/contest/706/problem/B Tags: binary search, dp, implementation, *1100 Ref: https://codeforces.com/contest/706/submission/91302380

---
+++ codeforces/706/B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define space " "
+#define TLE_na_ho ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE 1024
+#define MOD 1000000007
+#define pb push_back
+#define fi first
+#define se second
+#define f(a,n) for(ll i=a;i<n;i++)
+#define fe(a,n) for(ll i=a;i<=n;i++)
+#define ain(arr,n) for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+
+int main()
+{
+    TLE_na_ho;
+    int s;
+    cin>>s;
+    int a[s];
+    ain(a,s);
+    sort(a,a+s);
+    int k,e;
+    cin>>k;
+    while(k--)
+    {
+    	cin>>e;
+    	int ans=upper_bound(a,a+s,e)-a;
+    	cout<<ans<<endl;
+    }
+
+}
