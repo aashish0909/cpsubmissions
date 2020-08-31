8431cff1f4a30eb3cc68a19b12dc2bdf92fb9134
Aashish Illa <illa.aashish999@gmail.com>
2020-08-31 15:31:00 +0530

    Add solution for problem `A - Double Cola` Link: https://codeforces.com/contest/82/problem/A Tags: implementation, math, *1100 Ref: https://codeforces.com/contest/82/submission/91479571

---
+++ codeforces/82/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define space " "
+#define TLE_na_ho ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE 1024
+#define MOD 1000000007
+#define pb push_back
+#define fi first
+#define se second
+#define f(a,n) for(ll i=a;i<n;i++)
+#define fe(a,n) for(ll i=a;i<=n;i++)
+#define ain(arr,n) for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+
+int main()
+{
+    TLE_na_ho;
+    int n;
+    cin>>n;
+    int p=1;
+    while(p*5<n)
+    {
+    	n-=p*5;
+    	p*=2;
+    }
+    int ans=(n-1)/p;
+    string who[]={"Sheldon","Leonard","Penny","Rajesh","Howard"};
+    cout<<who[ans];
+
+}
