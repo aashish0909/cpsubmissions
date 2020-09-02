da115998eb92c1ca777f8161c79ff204d6f3de8e
Aashish Illa <illa.aashish999@gmail.com>
2020-09-02 11:41:00 +0530

    Add solution for problem `B - Sereja and Suffixes` Link: https://codeforces.com/contest/368/problem/B Tags: data structures, dp, *1100 Ref: https://codeforces.com/contest/368/submission/91615878

---
+++ codeforces/368/B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll =                   long long int;
+using pll =                  pair<ll, ll>;
+#define endl                 "\n"
+#define space                " "
+#define TLE_na_ho            ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE             1024
+#define MOD                  1000000007
+#define pb                   push_back
+#define fi                   first
+#define se                   second
+#define f(a,n)               for(ll i=a;i<n;i++)
+#define fe(a,n)              for(ll i=a;i<=n;i++)
+#define fr(a,n)              for(ll i=a;i>=n;i--)
+#define ain(arr,n)           for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n)          for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+
+int main()
+{
+    TLE_na_ho;
+	int n,m;
+	cin>>n>>m;
+	int a[n],b[n];
+	ain(a,n);
+	set<int> di;
+	fr(n-1,0){
+		di.insert(a[i]);
+		b[i]=di.size();
+	}  
+	int x;
+	while(m--){
+		cin>>x;
+		cout<<b[x-1]<<endl;
+	}
+}
