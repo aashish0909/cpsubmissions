ed6d063ac1896b1e9d236a9184514d66e9dde0be
Aashish Illa <illa.aashish999@gmail.com>
2020-09-08 12:00:00 +0530

    Add solution for problem `A - Bad Ugly Numbers` Link: https://codeforces.com/contest/1326/problem/A Tags: constructive algorithms, number theory, *1000 Ref: https://codeforces.com/contest/1326/submission/92186846

---
+++ codeforces/1326/A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll =                   long long int;
+using pll =                  pair<ll, ll>;
+#define endl                 "\n"
+#define space                " "
+#define TLE_na_ho            ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE             1024
+#define MOD                  1000000007
+#define pb                   push_back
+#define fi                   first
+#define se                   second
+#define f(a,n)               for(ll i=a;i<n;i++)
+#define fe(a,n)              for(ll i=a;i<=n;i++)
+#define fr(a,n)              for(ll i=a;i>=n;i--)
+#define ain(arr,n)           for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n)          for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+
+int main()
+{
+    TLE_na_ho;
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        if(n == 1)
+        {
+            cout << "-1" << endl;
+            continue;
+        }
+        cout << "2";
+        f(0, n - 1)
+        cout << "3";
+
+        cout << endl;
+
+
+    }
+
+}
