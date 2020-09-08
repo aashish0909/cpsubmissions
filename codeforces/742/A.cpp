b75a56a499a0d4bf31ec88b4ba3892a96486fc30
Aashish Illa <illa.aashish999@gmail.com>
2020-09-08 11:34:00 +0530

    Add solution for problem `A - Arpa’s hard exam and Mehrdad’s naive cheat` Link: https://codeforces.com/contest/742/problem/A Tags: implementation, math, number theory, *1000 Ref: https://codeforces.com/contest/742/submission/92185316

---
+++ codeforces/742/A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll =                   long long int;
+using pll =                  pair<ll, ll>;
+#define endl                 "\n"
+#define space                " "
+#define TLE_na_ho            ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE             1024
+#define MOD                  1000000007
+#define pb                   push_back
+#define fi                   first
+#define se                   second
+#define f(a,n)               for(ll i=a;i<n;i++)
+#define fe(a,n)              for(ll i=a;i<=n;i++)
+#define fr(a,n)              for(ll i=a;i>=n;i--)
+#define ain(arr,n)           for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n)          for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+
+int main()
+{
+    TLE_na_ho;
+    ll n;
+    cin >> n;
+    if(n==0)
+    {
+    	cout<<"1";
+    	return 0;
+    }
+    ll s = n % 4;
+    switch(s)
+    {
+    case 0 :
+        cout << "6";
+        break;
+    case 1 :
+        cout << "8";
+        break;
+    case 2 :
+        cout << "4";
+        break;
+    case 3:
+        cout << "2";
+        break;
+    }
+
+}
