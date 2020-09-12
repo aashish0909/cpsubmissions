081da0be789be6bab82379280581e332dfd7c24c
Aashish Illa <illa.aashish999@gmail.com>
2020-09-12 20:23:00 +0530

    Add solution for problem `D - Anti-Sudoku` Link: https://codeforces.com/contest/1335/problem/D Tags: constructive algorithms, implementation, *1300 Ref: https://codeforces.com/contest/1335/submission/92618863

---
+++ codeforces/1335/D.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll =                   long long int;
+using pll =                  pair<ll, ll>;
+#define endl                 "\n"
+#define space                " "
+#define TLE_na_ho            ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE             1024
+#define MOD                  1000000007
+#define pb                   push_back
+#define fi                   first
+#define se                   second
+#define rep(i,a,n)           for(ll i=a;i<n;i++)
+#define repe(i,a,n)          for(ll i=a;i<=n;i++)
+#define repr(i,a,n)          for(ll i=a;i>=n;i--)
+#define ain(arr,n)           for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n)          for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+
+int main()
+{
+    TLE_na_ho;
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+    	int n=9;
+        while(n--)
+        {
+        	string s;
+        	cin>>s;
+        	for(auto i:s)
+        	{
+        		if(i=='1')
+        			cout<<'2';
+        		else
+        			cout<<i;
+        	}
+        	cout<<endl;
+        }
+    }
+
+}
