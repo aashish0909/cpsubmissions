a4b34c45ff2a07de5498aa04e477a900b0fbb993
Aashish Illa <illa.aashish999@gmail.com>
2020-09-15 17:22:00 +0530

    Add solution for problem `B - Construct the String` Link: https://codeforces.com/contest/1335/problem/B Tags: constructive algorithms, *900 Ref: https://codeforces.com/contest/1335/submission/92897735

---
+++ codeforces/1335/B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll =                   long long int;
+using pll =                  pair<ll, ll>;
+#define endl                 "\n"
+#define space                " "
+#define TLE_na_ho            ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE             1024
+#define MOD                  1000000007
+#define pb                   push_back
+#define fi                   first
+#define se                   second
+#define rep(i,a,n)           for(ll i=a;i<n;i++)
+#define repe(i,a,n)          for(ll i=a;i<=n;i++)
+#define repr(i,a,n)          for(ll i=a;i>=n;i--)
+#define ain(arr,n)           for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n)          for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]
+
+
+int main()
+{
+    TLE_na_ho;
+    //READ THE QUESTION PROPERLY!!
+    ll t;
+    //t = 1;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        ll a,b;
+        cin>>a>>b;
+        char ans[n];
+        for(ll i=0;i<n;i++)
+        	ans[i]='a'+i%b;
+        aout(ans,n);
+        cout<<endl;
+    }
+
+}
