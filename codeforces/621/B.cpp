93f8c218348399e77f8abb56c092620891ef3d25
Aashish Illa <illa.aashish999@gmail.com>
2020-09-18 20:07:00 +0530

    Add solution for problem `B - Wet Shark and Bishops` Link: https://codeforces.com/contest/621/problem/B Tags: combinatorics, implementation, *1300 Ref: https://codeforces.com/contest/621/submission/93126933

---
+++ codeforces/621/B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using   ll  =       long long int;
+using   pll =       pair<ll, ll>;
+using   vll =       vector<ll>;
+#define endl        "\n"
+#define space       " "
+#define TLE_na_ho   ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE    1024
+#define MOD         1000000007
+#define pb          push_back
+#define fi          first
+#define se          second
+#define all(v)      v.begin(),v.end()
+#define f(n)        for(ll i=0;i<n;i++)
+#define rep(i,a,n)  for(ll i=a;i<n;i++)
+#define repe(i,a,n) for(ll i=a;i<=n;i++)
+#define repr(i,a,n) for(ll i=a;i>=n;i--)
+#define ain(arr,n)  for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+ll testcases;
+
+int main()
+{
+    TLE_na_ho;
+    //READ THE QUESTION PROPERLY!!
+    testcases=1;
+  //  cin>>testcases;
+    while(testcases--){
+        ll n;
+        cin>>n;
+        ll x[n],y[n];
+        map<ll,ll> add,subs;
+        f(n)
+        {
+        	cin>>x[i]>>y[i];
+        	add[x[i]+y[i]]++;
+        	subs[x[i]-y[i]]++;
+        }
+        ll sum=0;
+        for(auto i : add)
+        {
+        	if(i.se>=2)
+        	{
+        		sum+=(i.se*(i.se-1))/2;
+        	}
+        }
+        for(auto i : subs)
+        {
+        	if(i.se>=2)
+        	{
+        		sum+=(i.se*(i.se-1))/2;
+        	}
+        }
+
+        cout<<sum;
+    }
+
+}
