bcd8a1e36d443e2863a8347d9423d757c87f5536
Aashish Illa <illa.aashish999@gmail.com>
2020-09-20 20:59:00 +0530

    Add solution for problem `A - Keyboard` Link: https://codeforces.com/contest/474/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/474/submission/93348466

---
+++ codeforces/474/A.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using   ll  =       long long int;
+using   pll =       pair<ll, ll>;
+using   vll =       vector<ll>;
+#define endl        "\n"
+#define space       " "
+#define TLE_na_ho   ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE    1024
+#define MOD         1000000007
+#define pb          push_back
+#define fi          first
+#define se          second
+#define all(v)      v.begin(),v.end()
+#define f(n)        for(ll i=0;i<n;i++)
+#define rep(i,a,n)  for(ll i=a;i<n;i++)
+#define repe(i,a,n) for(ll i=a;i<=n;i++)
+#define repr(i,a,n) for(ll i=a;i>=n;i--)
+#define ain(arr,n)  for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+ll testcases;
+
+int main()
+{
+    TLE_na_ho;
+    //READ THE QUESTION PROPERLY!!
+    testcases = 1;
+    // cin>>testcases;
+    while(testcases--)
+    {
+        char in;
+        cin >> in;
+        string s;
+        cin >> s;
+        string kb[3] = {"qwertyuiop", "asdfghjkl;", "zxcvbnm,./"};
+        if(in == 'L')
+        {
+            rep(i, 0, s.length())
+            {
+                char test = s[i];
+                rep(j, 0, 3)
+                {
+                    rep(k, 0, 10)
+                    {
+                        if(kb[j][k] == test)
+                            cout << kb[j][k + 1];
+                    }
+                }
+            }
+        }
+        else
+        {
+            rep(i, 0, s.length())
+            {
+                char test = s[i];
+                rep(j, 0, 3)
+                {
+                    rep(k, 0, 10)
+                    {
+                        if(kb[j][k] == test)
+                            cout << kb[j][k - 1];
+                    }
+                }
+            }
+        }
+    }
+
+}
