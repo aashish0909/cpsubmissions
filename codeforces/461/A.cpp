087e3ba370e4775602f3e7fbf1ae917e3537e655
Aashish Illa <illa.aashish999@gmail.com>
2020-10-01 22:33:00 +0530

    Add solution for problem `A - Appleman and Toastman` Link: https://codeforces.com/contest/461/problem/A Tags: greedy, sortings, *1200 Ref: https://codeforces.com/contest/461/submission/94427260

---
+++ codeforces/461/A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using   ll  =       long long int;
+using   pll =       pair<ll, ll>;
+using   vll =       vector<ll>;
+#define endl        "\n"
+#define space       " "
+#define TLE_na_ho   ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE    1024
+#define MOD         1000000007
+#define pb          push_back
+#define fi          first
+#define se          second
+#define all(v)      v.begin(),v.end()
+#define f(n)        for(ll i=0;i<n;i++)
+#define rep(i,a,n)  for(ll i=a;i<n;i++)
+#define repe(i,a,n) for(ll i=a;i<=n;i++)
+#define repr(i,a,n) for(ll i=a;i>=n;i--)
+#define ain(arr,n)  for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+ll testcases;
+
+int main()
+{
+    TLE_na_ho;
+    //READ THE QUESTION PROPERLY!!
+    testcases = 1;
+    //cin>>testcases;
+    while(testcases--)
+    {
+        ll n;
+        cin >> n;
+        vll a(n);
+        ain(a, n);
+        ll sum = 0;
+        f(n)
+        sum += a[i];
+        //cout<<sum<<endl;
+        sort(all(a));
+        ll ans = sum;
+        ll i = 0;
+        while(i < n-1)
+        {
+            ans += sum;
+            sum=sum-a[i];
+            i++;
+        }
+        cout << ans << endl;
+    }
+
+}
