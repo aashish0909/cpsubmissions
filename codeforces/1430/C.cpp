972a5ff3a568dcff9b5d9f2a1f1e780f58fd8aad
Aashish Illa <illa.aashish999@gmail.com>
2020-10-11 16:47:00 +0530

    Add solution for problem `C - Numbers on Whiteboard` Link: https://codeforces.com/contest/1430/problem/C Tags: constructive algorithms, data structures, greedy, implementation, math, *1000 Ref: https://codeforces.com/contest/1430/submission/95253793

---
+++ codeforces/1430/C.cpp
@@ -0,0 +1,56 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using   ll  =       long long int;
+using   pll =       pair<ll, ll>;
+using   vll =       vector<ll>;
+#define endl        "\n"
+#define space       " "
+#define TLE_na_ho   ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE    1024
+#define MOD         1000000007
+#define pb          push_back
+#define ff          first
+#define ss          second
+#define all(v)      v.begin(),v.end()
+#define f(n)        for(ll i=0;i<n;i++)
+#define rep(i,a,n)  for(ll i=a;i<n;i++)
+#define repe(i,a,n) for(ll i=a;i<=n;i++)
+#define repr(i,a,n) for(ll i=a;i>=n;i--)
+#define ain(arr,n)  for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+ll testcases;
+
+int main()
+{
+    TLE_na_ho;
+    //READ THE QUESTION PROPERLY!!
+    //testcases=1;
+    cin >> testcases;
+    while(testcases--)
+    {
+        ll n;
+        cin >> n;
+        cout << 2 << endl;
+        ll ans = n;
+        if(n == 2)
+        {
+            cout << 1 << space << 2 << endl;
+            continue;
+        }
+        cout << n << space << n - 2 << endl;
+        cout << n - 1 << space << n - 1 << endl;
+
+        ll p = n - 3;
+        n--;
+        f(p)
+        {
+            cout << n << space << n - 2 << endl;
+            n--;
+        }
+    }
+
+}
