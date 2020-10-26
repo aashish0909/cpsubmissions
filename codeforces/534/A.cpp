d900ba050701117c7d328f855404b34d8748a9ed
Aashish Illa <illa.aashish999@gmail.com>
2020-10-26 19:06:00 +0530

    Add solution for problem `A - Exam` Link: https://codeforces.com/contest/534/problem/A Tags: constructive algorithms, implementation, math, *1100 Ref: https://codeforces.com/contest/534/submission/96781993

---
+++ codeforces/534/A.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using   ll  =       long long int;
+using   pll =       pair<ll, ll>;
+using   vll =       vector<ll>;
+#define endl        "\n"
+#define ssp         " "
+#define nla         cout<<"\n"
+#define TLE_na_ho   ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define MAX_SIZE    1024
+#define MOD         1000000007
+#define pb          push_back
+#define ff          first
+#define ss          second
+#define all(v)      v.begin(),v.end()
+#define f(n)        for(ll i=0;i<n;i++)
+#define rep(i,a,n)  for(ll i=a;i<n;i++)
+#define repe(i,a,n) for(ll i=a;i<=n;i++)
+#define repr(i,a,n) for(ll i=a;i>=n;i--)
+#define ain(arr,n)  for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<space
+
+/*           DEBUG           */
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+/*           DEBUG           */
+
+
+
+
+
+
+ll testcases;
+
+int main()
+{
+    TLE_na_ho;
+    //READ THE QUESTION PROPERLY!!
+    testcases=1;
+    //cin>>testcases;
+    while(testcases--){
+        ll n;
+        cin>>n;
+        if(n<=2)
+        	cout<<1<<endl<<1;
+        else if(n==3)
+        	cout<<2<<endl<<1<<ssp<<3<<endl;
+        else if(n==4)
+        	cout<<4<<endl<<3<<ssp<<1<<ssp<<4<<ssp<<2<<endl;
+        else
+        {
+        	cout<<n<<endl;
+        	for(ll i=1;i<=n;i+=2)
+        		cout<<i<<ssp;
+        	for(ll i=2;i<=n;i+=2)
+        		cout<<i<<ssp;
+        	nla;
+        }
+    }
+
+}
