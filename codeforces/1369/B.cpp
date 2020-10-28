84706c7d41511cf8daf18146c29625fc3aeb210a
Aashish Illa <illa.aashish999@gmail.com>
2020-10-28 17:53:00 +0530

    Add solution for problem `B - AccurateLee` Link: https://codeforces.com/contest/1369/problem/B Tags: greedy, implementation, strings, *1200 Ref: https://codeforces.com/contest/1369/submission/96992652

---
+++ codeforces/1369/B.cpp
@@ -0,0 +1,173 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using   ll        = long long int;
+using   pll       = pair<ll, ll>;
+using   vll       = vector<ll>;
+ll const max_size = 1024;
+ll const mod      = 1e9 + 7;
+ll const maxn     = 1e5 + 1;
+#define endl        "\n"
+#define ssp         " "
+#define n_l         cout<<"\n"
+#define TLE_na_ho   ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define pb          push_back
+#define ff          first
+#define ss          second
+#define all(v)      v.begin(),v.end()
+#define f(n)        for(ll i=0;i<n;i++)
+#define rep(i,a,n)  for(ll i=a;i<n;i++)
+#define repe(i,a,n) for(ll i=a;i<=n;i++)
+#define repr(i,a,n) for(ll i=a;i>=n;i--)
+#define ain(arr,n)  for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]
+
+/*
+░█▀▀▄ ░█▀▀▀ ░█▀▀█ ░█─░█ ░█▀▀█
+░█─░█ ░█▀▀▀ ░█▀▀▄ ░█─░█ ░█─▄▄
+░█▄▄▀ ░█▄▄▄ ░█▄▄█ ─▀▄▄▀ ░█▄▄█*/
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x)
+{
+    cerr << x;
+}
+void __print(long long x)
+{
+    cerr << x;
+}
+void __print(unsigned x)
+{
+    cerr << x;
+}
+void __print(unsigned long x)
+{
+    cerr << x;
+}
+void __print(unsigned long long x)
+{
+    cerr << x;
+}
+void __print(float x)
+{
+    cerr << x;
+}
+void __print(double x)
+{
+    cerr << x;
+}
+void __print(long double x)
+{
+    cerr << x;
+}
+void __print(char x)
+{
+    cerr << '\'' << x << '\'';
+}
+void __print(const char *x)
+{
+    cerr << '\"' << x << '\"';
+}
+void __print(const string &x)
+{
+    cerr << '\"' << x << '\"';
+}
+void __print(bool x)
+{
+    cerr << (x ? "true" : "false");
+}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template<typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x) cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print()
+{
+    cerr << "]\n";
+}
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v)) cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+/*
+░█▀▀▄ ░█▀▀▀ ░█▀▀█ ░█─░█ ░█▀▀█
+░█─░█ ░█▀▀▀ ░█▀▀▄ ░█─░█ ░█─▄▄
+░█▄▄▀ ░█▄▄▄ ░█▄▄█ ─▀▄▄▀ ░█▄▄█*/
+
+
+ll testcases;
+
+int main()
+{
+    TLE_na_ho;
+    //READ THE QUESTION PROPERLY!!
+    //testcases=1;
+    cin >> testcases;
+    while(testcases--)
+    {
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+        ll one = 0;
+        ll zero = 0;
+        bool f = false;
+        debug(s[0]-'0',s[n-1]-'0');
+        f(n - 1)
+        {
+            if((s[i]-'0') > (s[i + 1]-'0')){
+                f = true;
+                break;
+            }
+        }
+        if(!f)
+        {
+            cout << s << endl;
+            continue;
+        }
+        f(n)
+        {
+            if(s[i] == '1')
+                break;
+            else
+                zero++;
+        }
+        repr(i, s.length() - 1, 0)
+        {
+            if(s[i] == '0')
+                break;
+            else
+                one++;
+        }
+        f(zero + 1)
+        cout << 0;
+        f(one)
+        cout << 1;
+        n_l;
+
+    }
+
+}
