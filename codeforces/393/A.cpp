18c4d83fca77fddd27a67e24d0809a6ba9c919cb
Aashish Illa <illa.aashish999@gmail.com>
2020-11-06 15:03:00 +0530

    Add solution for problem `A - Nineteen` Link: https://codeforces.com/contest/393/problem/A Tags: Ref: https://codeforces.com/contest/393/submission/97765520

---
+++ codeforces/393/A.cpp
@@ -0,0 +1,214 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using   ll        = long long int;
+using   pll       = pair<ll, ll>;
+using   vll       = vector<ll>;
+ll const max_size = 1024;
+ll const mod      = 1e9 + 7;
+ll const maxn     = 1e5 + 1;
+#define endl        "\n"
+#define ssp         " "
+#define n_l         cout<<"\n"
+#define TLE_na_ho   ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define pb          push_back
+#define ff          first
+#define ss          second
+#define all(v)      v.begin(),v.end()
+#define f(n)        for(ll i=0;i<n;i++)
+#define rep(i,a,n)  for(ll i=a;i<n;i++)
+#define repe(i,a,n) for(ll i=a;i<=n;i++)
+#define repr(i,a,n) for(ll i=a;i>=n;i--)
+#define ain(arr,n)  for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<ssp
+
+/*
+░█▀▀▄ ░█▀▀▀ ░█▀▀█ ░█─░█ ░█▀▀█
+░█─░█ ░█▀▀▀ ░█▀▀▄ ░█─░█ ░█─▄▄
+░█▄▄▀ ░█▄▄▄ ░█▄▄█ ─▀▄▄▀ ░█▄▄█*/
+void __print(int x)
+{
+    cerr << x;
+}
+void __print(long x)
+{
+    cerr << x;
+}
+void __print(long long x)
+{
+    cerr << x;
+}
+void __print(unsigned x)
+{
+    cerr << x;
+}
+void __print(unsigned long x)
+{
+    cerr << x;
+}
+void __print(unsigned long long x)
+{
+    cerr << x;
+}
+void __print(float x)
+{
+    cerr << x;
+}
+void __print(double x)
+{
+    cerr << x;
+}
+void __print(long double x)
+{
+    cerr << x;
+}
+void __print(char x)
+{
+    cerr << '\'' << x << '\'';
+}
+void __print(const char *x)
+{
+    cerr << '\"' << x << '\"';
+}
+void __print(const string &x)
+{
+    cerr << '\"' << x << '\"';
+}
+void __print(bool x)
+{
+    cerr << (x ? "true" : "false");
+}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template<typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x) cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print()
+{
+    cerr << "]\n";
+}
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v)) cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+/*
+░█▀▀▄ ░█▀▀▀ ░█▀▀█ ░█─░█ ░█▀▀█
+░█─░█ ░█▀▀▀ ░█▀▀▄ ░█─░█ ░█─▄▄
+░█▄▄▀ ░█▄▄▄ ░█▄▄█ ─▀▄▄▀ ░█▄▄█*/
+
+
+ll testcases;
+
+int main()
+{
+    TLE_na_ho;
+    //READ THE QUESTION PROPERLY!!
+    testcases = 1;
+    //cin>>testcases;
+    while(testcases--)
+    {
+        string s;
+        cin >> s;
+        vll c(4);
+        f(s.length())
+        {
+            if(s[i] == 'n')
+                c[0]++;
+            if(s[i] == 'i')
+                c[1]++;
+            if(s[i] == 'e')
+                c[2]++;
+            if(s[i] == 't')
+                c[3]++;
+        }
+        if(c[0] < 3 || c[1] < 1 || c[2] < 3 || c[3] < 1)
+        {
+            cout << 0;
+            return 0;
+        }
+        ll cnt = 0;
+        string ans;
+        if(c[0] > 0)
+        {
+            ans += 'n';
+            c[0]--;
+        }
+        while(c[0] > 0 && c[1] > 0 && c[2] > 0 && c[3] > 0)
+        {
+            if(c[1] > 0)
+            {
+                ans += 'i';
+                c[1]--;
+            }
+            if(c[0] > 0)
+            {
+                ans += 'n';
+                c[0]--;
+            }
+
+            if(c[2] > 0)
+            {
+                ans += 'e';
+                c[2]--;
+            }
+            if(c[3] > 0)
+            {
+                ans += 't';
+                c[3]--;
+            }
+            if(c[2] > 0)
+            {
+                ans += 'e';
+                c[2]--;
+            }
+            if(c[2] > 0)
+            {
+                ans += 'e';
+                c[2]--;
+            }
+            if(c[0] > 0)
+            {
+                ans += 'n';
+                c[0]--;
+            }
+        }
+        debug(ans);
+        debug(ans[ans.size() - 7]);
+        f(ans.length() - 7)
+        {
+            if(ans[i] == 'n')
+            {
+                if(ans[i + 1] == 'i' && ans[i + 2] == 'n' && ans[i + 3] == 'e' &&
+                        ans[i + 4] == 't' && ans[i + 5] == 'e' && ans[i + 6] == 'e' && ans[i + 7] == 'n')
+                {
+                    cnt++;
+                    i+=6;
+                }
+            }
+            debug(i);
+        }
+        cout << cnt;
+    }
+
+}
