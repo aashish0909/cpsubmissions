3e0d2d795cb3fd9584ac41ee00601e075cf1f43b
Aashish Illa <illa.aashish999@gmail.com>
2020-11-27 15:49:00 +0530

    Add solution for problem `A - Special Permutation` Link: https://codeforces.com/contest/1454/problem/A Tags: constructive algorithms, probabilities, *800 Ref: https://codeforces.com/contest/1454/submission/99705255

---
+++ codeforces/1454/A.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+using vll = vector<ll>;
+ll const mod = 1e9 + 7;
+#define endl "\n"
+#define pb push_back
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define f(n) for(ll i=0;i<n;i++)
+#define rep(i,a,n) for(ll i=a;i<n;i++)
+#define repe(i,a,n) for(ll i=a;i<=n;i++)
+#define repr(i,a,n) for(ll i=a;i>n;i--)
+#define ain(arr,n) for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<" "
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+
+ll testcases;
+
+int main()
+{
+    aashish_999;
+    //READ THE QUESTION PROPERLY!!
+    //testcases=1;
+    cin >> testcases;
+    while(testcases--)
+    {
+        ll n;
+        cin >> n;
+        ll p = n / 2;
+        if(n & 1)
+            p++;
+        for(ll i = n; i > p; i--)
+        {
+            cout << i << " ";
+        }
+        for(ll i = 1; i <= p; i++)
+            cout << i << " ";
+        cout << endl;
+    }
+
+}
