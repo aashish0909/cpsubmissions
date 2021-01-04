1407011b29f1cc094678894123d8d45c400170bc
Aashish Illa <illa.aashish999@gmail.com>
2021-01-04 07:51:00 +0530

    Add solution for problem `A - Two Bases` Link: https://codeforces.com/contest/602/problem/A Tags: brute force, implementation, *1100 Ref: https://codeforces.com/contest/602/submission/103138210

---
+++ codeforces/602/A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+using vll = vector<ll>;
+ll const mod = 1e9 + 7;
+#define endl "\n"
+#define pb push_back
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define f(n) for(ll i=0;i<n;i++)
+#define rep(i,a,n) for(ll i=a;i<n;i++)
+#define repe(i,a,n) for(ll i=a;i<=n;i++)
+#define repr(i,a,n) for(ll i=a;i>=n;i--)
+#define ain(arr,n) for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<" "
+#define ordered_set tree<ll, null_type,less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update>
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+    ll n, b1;
+    ll m, b2;
+    cin >> n >> b1;
+    ll a[n];
+    ain(a, n);
+    cin >> m >> b2;
+    ll b[m];
+    ain(b, m);
+    ll n1 = 0;
+    ll n2 = 0;
+    f(n)
+    n1 += a[i] * pow(b1, n - i - 1);
+
+    if(b2 == 10)
+        f(m)
+        n2 += b[i] * pow(10, m - i - 1);
+    else
+        f(m)
+        n2 += b[i] * pow(b2, m - 1 - i);
+
+    //cout << n1 << " " << n2 << endl;
+
+    if(n1 == n2)
+        cout << "=";
+
+    else if(n1 > n2)
+        cout << ">";
+    else
+        cout << "<";
+}
+
+int main()
+{
+    aashish_999;
+
+    ll testcases = 1;
+    //  cin >> testcases;
+    while(testcases--)
+    {
+        solve();
+    }
+}
