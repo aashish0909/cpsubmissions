301646e4c9efc186d6ce4e926d7f097da224776b
Aashish Illa <illa.aashish999@gmail.com>
2021-01-05 18:36:00 +0530

    Add solution for problem `A - Divisibility Problem` Link: https://codeforces.com/contest/1328/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1328/submission/103383063

---
+++ codeforces/1328/A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long int;
+using vll = vector<ll>;
+ll const mod = 1e9 + 7;
+#define endl "\n"
+#define pb push_back
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define f(n) for(ll i=0;i<n;i++)
+#define rep(i,a,n) for(ll i=a;i<n;i++)
+#define repe(i,a,n) for(ll i=a;i<=n;i++)
+#define repr(i,a,n) for(ll i=a;i>=n;i--)
+#define ain(arr,n) for(ll i1=0;i1<n;i1++ ) cin>>arr[i1]
+#define aout(arr,n) for(ll i1=0;i1<n;i1++ ) cout<<arr[i1]<<" "
+#define ordered_set tree<ll, null_type,less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update>
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    if (a > b)
+        cout << (ll)ceil((double)a / b)*b - a << endl;
+    else
+        cout << b - a << endl;
+}
+
+int main()
+{
+    aashish_999;
+
+    ll testcases = 1;
+    cin >> testcases;
+    while (testcases--)
+    {
+        solve();
+    }
+}
