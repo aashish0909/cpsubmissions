4fb1ae332934ada1e6e11b3918caeb723cddbb25
Aashish Illa <illa.aashish999@gmail.com>
2021-02-24 15:08:00 +0530

    Add solution for problem `A - Rating Goal` Link: https://atcoder.jp/contests/abc076/tasks/abc076_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc076/submissions/20461910

---
+++ atcoder/abc076/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+	// Understand the problem.
+	// Devise a plan.
+	// Carry out the plan.
+	// Look back.
+
+	int a, b;
+	cin >> a >> b;
+	cout << 2 * b - a;
+}
+
+int32_t main()
+{
+	aashish_999;
+
+	int testcases = 1;
+	//cin >> testcases;
+	while (testcases--)
+	{
+		solve();
+	}
+}
