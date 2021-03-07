b30c0dca0205d2e420a0a2b0b7adce730c7a801f
Aashish Illa <illa.aashish999@gmail.com>
2021-03-07 17:57:00 +0530

    Add solution for problem `C - Squared Error` Link: https://atcoder.jp/contests/abc194/tasks/abc194_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc194/submissions/20756513

--- atcoder/abc194/C.cpp
+++ atcoder/abc194/C.cpp
@@ -1,20 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() 
+int main()
 {
-long long s=0,s1=0;
-int n,i;cin>>n;
-vector<int>a(n);
-   
-   for(i=0;i<n;i++)
-   {cin>>a[i];
-   s=s+1ll*a[i];}
-   for(i=0;i<n;i++)
-   {
-       s1=s1+1ll*(n-1)*a[i]*a[i]-1ll*2*a[i]*(s-a[i]);
-       s=s-a[i];
-   }
-   
-   cout<<s1;
+    long long s = 0, s1 = 0;
+    int n, i;
+    cin >> n;
+    int a[n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        s = s + a[i];
+    }
+    s1 = 1ll*(n - 1) * (a[n - 1] * a[n - 1]);
+    for (i = 0; i < n - 1; i++)
+    {
+        s = s - 1ll*a[i];
+        s1 = s1 + (1ll*(n - 1) * a[i] * a[i]) - (1ll*2 * a[i] * s);
+    }
+
+    cout << s1;
 }
