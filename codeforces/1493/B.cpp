89ce67b4dc5634044e5081eabf79eb2f1b437a6e
Aashish Illa <illa.aashish999@gmail.com>
2021-03-07 10:19:00 +0530

    Add solution for problem `B - Planet Lapituletti` Link: https://codeforces.com/contest/1493/problem/B Tags: brute force, implementation, *1300 Ref: https://codeforces.com/contest/1493/submission/109305541

--- codeforces/1493/B.cpp
+++ codeforces/1493/B.cpp
@@ -4,111 +4,53 @@ using namespace std;
 #define int long long int
 #define endl "\n"
 #define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
-set<char> valid;
+set<int> valid;
+map<int, int> re;
+bool check(int ch, int cm, int h, int m) {
+    if (valid.find(ch / 10) == valid.end()) return false;
+    if (valid.find(ch % 10) == valid.end()) return false;
+    if (valid.find(cm / 10) == valid.end()) return false;
+    if (valid.find(cm % 10) == valid.end()) return false;
+    int mo = re[ch / 10], mt = re[ch % 10], ho = re[cm / 10], ht = re[cm % 10];
 
-int conv(string ch) {
-    int nh = 0;
-    for (auto i : ch)
-        nh = nh * 10 + (i - '0');
-    return nh;
-}
-
-bool check(string h, string m, string th, string tm) {
-    for (auto i : h)
-        if (valid.find(i) == valid.end())
-            return false;
-    for (auto i : m)
-        if (valid.find(i) == valid.end())
-            return false;
-    for (int i = 0; i < 2; i++)
-        if (h[i] == '2')
-            h[i] = '5';
-        else if (h[i] == '5')
-            h[i] = '2';
-    for (int i = 0; i < 2; i++)
-        if (m[i] == '2')
-            m[i] = '5';
-        else if (m[i] == '5')
-            m[i] = '2';
-
-    reverse(h.begin(), h.end());
-    reverse(m.begin(), m.end());
-    // cout << m << " " << tm << ":" << th << " " << h << ":::" << endl;
-    if (m > th || h > tm)
-        return false;
-    return true;
+    return ((10 * ht + ho) < h && (10 * mt + mo) < m);
 }
 
 void solve()
 {
     //read the question properly
 
-    string h, m;
+    int h, m;
     cin >> h >> m;
-    int cch = stoi(h), ccm = stoi(m);
-    --cch, --ccm;
-    h = to_string(cch), m = to_string(ccm);
-    string ch, cm;
+    int ch, cm;
     char s;
-    for (int i = 0; i < 2; i++) {
-        char x;
-        cin >> x;
-        ch.push_back(x);
+    cin >> ch >> s >> cm;
+    // cout << ch << " " << cm << endl;
+    while (!check(ch, cm, h, m)) {
+        ++cm;
+        if (cm >= m) {
+            cm = 0, ++ch;
+            if (ch >= h)
+                ch = 0;
+        }
+        // cout << "###" << ch / 10 << ch % 10 << ":" << cm / 10 << cm % 10 << endl;
     }
-    cin >> s;
-    for (int i = 0; i < 2; i++) {
-        char x;
-        cin >> x;
-        cm.push_back(x);
-    }
-    int nh = 0, nm = 0;
-    if (h.size() == 1) {
-        string temp = h;
-        h = '0';
-        h += temp;
-    }
-    if (m.size() == 1) {
-        string temp = m;
-        m = '0';
-        m += temp;
-    }
-    for (auto i : h)
-        nh = nh * 10 + (i - '0');
-    for (auto i : m)
-        nm = nm * 10 + (i - '0');
-    // cout << nh << " " << nm << endl;
-
-    while (check(ch, cm, h, m) == 0) {
-        int ph = conv(ch);
-        int pm = conv(cm);
-        ++pm;
-        if (pm == nm + 1)
-            pm = 0, ++ph;
-        if (ph == nh + 1)
-            ph = 0;
-        ch = "";
-        cm = "";
-        if (ph < 10)
-            ch += '0';
-        ch += to_string(ph);
-        if (pm < 10)
-            cm += '0';
-        cm += to_string(pm);
-        // cout << ch << "::" << cm << endl;
-    }
-
-    cout << ch << ":" << cm << endl;
-
+    cout << ch / 10 << ch % 10 << ":" << cm / 10 << cm % 10 << endl;
 }
 
 int32_t main()
 {
     aashish_999;
-    valid.insert('0');
-    valid.insert('1');
-    valid.insert('2');
-    valid.insert('5');
-    valid.insert('8');
+    valid.insert(0);
+    valid.insert(1);
+    valid.insert(2);
+    valid.insert(5);
+    valid.insert(8);
+    re[1] = 1;
+    re[0] = 0;
+    re[2] = 5;
+    re[5] = 2;
+    re[8] = 8;
     int testcases = 1;
     cin >> testcases;
     while (testcases--)
