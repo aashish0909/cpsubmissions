b7d8a60a613bdb6231e6adcd7b3ecee5ce769f07
Aashish Illa <illa.aashish999@gmail.com>
2021-03-16 15:11:00 +0530

    Add solution for problem `A - Health M Death` Link: https://atcoder.jp/contests/abc195/tasks/abc195_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc195/submissions/20979964

---
+++ atcoder/abc195/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+	//read the question properly
+	int m, h;
+	cin >> m >> h;
+	if (h % m == 0)
+		cout << "Yes";
+	else cout << "No";
+}
+
+int32_t main()
+{
+	aashish_999;
+
+	int testcases = 1;
+	//cin >> testcases;
+	while (testcases--)
+	{
+		solve();
+	}
+}
