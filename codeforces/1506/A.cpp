e9b7a16da47ceee519ebe55427c2a0787b8b6648
Aashish Illa <illa.aashish999@gmail.com>
2021-03-25 20:43:00 +0530

    Add solution for problem `A - Strange Table` Link: https://codeforces.com/contest/1506/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1506/submission/111009360

--- codeforces/1506/A.cpp
+++ codeforces/1506/A.cpp
@@ -8,18 +8,39 @@ using namespace std;
 void solve()
 {
 	//read the question properly
+	int n, m, k;
+	cin >> n >> m >> k;
+	int row = k % n, col = 0;
+	if (row == 0)
+		row = n;
 
-	int n, m, x;
-	cin >> n >> m >> x;
-	--x;
-	int r = x % n;
-	int c = x / n;
-	cout << r*m + c + 1 << endl;
+	int st = 1;
+	int en = m;
+
+	while (st <= en) {
+		int mid = (st + en) / 2;
+		// cout << mid << endl;
+		if (row + (mid - 1)*n < k)
+			st = mid + 1;
+		else if (row + (mid - 1)*n == k) {
+			col = mid;
+			break;
+		}
+		else
+			en = mid - 1;
+	}
+
+	cout << (row - 1)*m + col << endl;
+
 }
 
 int32_t main()
 {
 	aashish_999;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
 
 	int testcases = 1;
 	cin >> testcases;
