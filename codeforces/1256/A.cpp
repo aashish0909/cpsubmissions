5dfb09f894fac2209e724cb64de150dc2dce6fea
Aashish Illa <illa.aashish999@gmail.com>
2021-04-17 17:03:00 +0530

    Add solution for problem `A - Payment Without Change` Link: https://codeforces.com/contest/1256/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/1256/submission/113325764

---
+++ codeforces/1256/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+	//read the question properly
+	int a, b, n, s;
+	cin >> a >> b >> n >> s;
+	if (s % n <= b && a * n + b >= s)
+		cout << "YES" << endl;
+	else cout << "NO" << endl;
+}
+
+int32_t main()
+{
+	aashish_999;
+
+	int testcases = 1;
+	cin >> testcases;
+	while (testcases--)
+	{
+		solve();
+	}
+}
