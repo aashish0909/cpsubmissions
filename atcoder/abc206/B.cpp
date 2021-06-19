d232351ec4d988ab6dc8ffbb802720fe519e3ff1
Aashish Illa <illa.aashish999@gmail.com>
2021-06-19 17:44:00 +0530

    Add solution for problem `B - Savings` Link: https://atcoder.jp/contests/abc206/tasks/abc206_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc206/submissions/23589397

---
+++ atcoder/abc206/B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+	//read the question properly
+	int day = 1;
+	int n;
+	cin >> n;
+	int sum = 0;
+	while ((sum + day) < n)
+		sum += (day++);
+	cout << day << endl;
+}
+
+int32_t main()
+{
+	aashish_999;
+
+	int testcases = 1;
+	//cin >> testcases;
+	while (testcases--)
+	{
+		solve();
+	}
+}
