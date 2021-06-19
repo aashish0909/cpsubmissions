13b5f07d2650d4db380892298ece3150bcf40e40
Aashish Illa <illa.aashish999@gmail.com>
2021-06-19 17:41:00 +0530

    Add solution for problem `A - Maxi-Buying` Link: https://atcoder.jp/contests/abc206/tasks/abc206_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc206/submissions/23587233

---
+++ atcoder/abc206/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+	//read the question properly
+
+	int n;
+	cin >> n;
+	int t = floor(1.08 * n);
+	cout << (t < 206 ? "Yay!" : (t == 206 ? "so-so" : ":("));
+}
+
+int32_t main()
+{
+	aashish_999;
+
+	int testcases = 1;
+	//cin >> testcases;
+	while (testcases--)
+	{
+		solve();
+	}
+}
