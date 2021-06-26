95ffaae9a1fce738fbd49a6fa80ffdb14ad78ab5
Aashish Illa <illa.aashish999@gmail.com>
2021-06-26 17:31:00 +0530

    Add solution for problem `A - Repression` Link: https://atcoder.jp/contests/abc207/tasks/abc207_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc207/submissions/23757167

---
+++ atcoder/abc207/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+	//read the question properly
+	vector<int> a(3);
+	for (auto &i : a) cin >> i;
+	sort(a.begin(), a.end());
+	cout << a[2] + a[1];
+}
+
+int32_t main()
+{
+	aashish_999;
+
+	int testcases = 1;
+	//cin >> testcases;
+	while (testcases--)
+	{
+		solve();
+	}
+}
