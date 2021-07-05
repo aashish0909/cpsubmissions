d7fe3626009366deebd00f17e258b10db7d8089f
Aashish Illa <illa.aashish999@gmail.com>
2021-07-05 16:43:00 +0530

    Add solution for problem `A - Rolling Dice` Link: https://atcoder.jp/contests/abc208/tasks/abc208_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc208/submissions/24010271

---
+++ atcoder/abc208/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+#define aashish_999 ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+void solve()
+{
+    //read the question properly
+
+    int a, b;
+    cin >> a >> b;
+    if (a <= b and b <= (a * 6)) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
+
+int32_t main()
+{
+    aashish_999;
+
+    int testcases = 1;
+    //cin >> testcases;
+    while (testcases--)
+    {
+        solve();
+    }
+}
